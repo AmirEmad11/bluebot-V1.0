44837a6a067242ab7b68e9b9396efb22d2821522
AmirEmad11 <92502500+AmirEmad11@users.noreply.github.com>
2026-01-14 17:46:53 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# ملف الإعدادات - لا تشارك هذا الملف مع أحد!
+# هذه البيانات حساسة جداً
+
+API_ID = 39067761
+API_HASH = "e6b5fae123a1e1d1858d13fc1b8ce684"
+PHONE = "+201153943941"
+PASSWORD = ""  # ضع كلمة المرور هنا إذا كان لديك 2FA
+BOT_TOKEN = "8156871874:AAH0T7tMCOWuyNnNoHCSSHR0H6VLr7I6RpM"
+CHANNEL_IDENTIFIER = "-1003296506024"  # ID القناة المستهدفة
+ADMIN_ID = 8213142311  # ID حساب الأدمن
